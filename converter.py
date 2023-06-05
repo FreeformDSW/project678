2e90e963e129158564b593121b9f78f1356f8072
FreeformDSW <dsw51560@student.dsw.edu.pl>
2023-06-05 23:07:17 +0200

    Rozbudowanie skryptu o wczytanie pliku JSON

--- converter.py
+++ converter.py
@@ -1,9 +1,21 @@
 import sys
+import json
 
-# ~~~~~~~~~~~Parsowanie~~~~~~~~~~~
+# ~~~~~~~~~~~ Parsowanie ~~~~~~~~~~~
 if len(sys.argv) != 3:
     print("Dozwolone sa tylko dwa argumenty")
 else:
     plik_wej = sys.argv[1]
     plik_wyj = sys.argv[2]
 
+
+# ~~~~~~~~~~~ Wczytanie pliku JSON i weryfikacja składni ~~~~~~~~~~~
+def podaj_plik_json(plik_wej):
+    try:
+        with open(plik_wej, 'r') as plik:
+            a = json.load(plik)
+            return a
+    except json.JSONDecodeError:
+        print("Taki plik nie istnieje, sprawdź nazwę")
+    except FileNotFoundError:
+        print("Taki plik nie istnieje")
