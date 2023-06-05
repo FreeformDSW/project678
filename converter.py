e463f3c01202cd3e63c27547005d81538ec7d9ba
FreeformDSW <dsw51560@student.dsw.edu.pl>
2023-06-05 22:53:32 +0200

    Dodanie głównego skryptu, na razie tylko z parsowaniem argumentów

---
+++ converter.py
@@ -0,0 +1,9 @@
+import sys
+
+# ~~~~~~~~~~~Parsowanie~~~~~~~~~~~
+if len(sys.argv) != 3:
+    print("Dozwolone sa tylko dwa argumenty")
+else:
+    plik_wej = sys.argv[1]
+    plik_wyj = sys.argv[2]
+
